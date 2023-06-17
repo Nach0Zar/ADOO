5996b6b821302265b896bacf15043e8103958ee1
Nach0Zar <ignaciozarlenga@hotmail.com>
2023-06-17 14:35:57 -0300

    Added ignore to intellij files and created RecordatorioDTO

--- src/modelos/dtos/RecordatorioDTO.java
+++ src/modelos/dtos/RecordatorioDTO.java
@@ -1,32 +1,34 @@
 package modelos.dtos;
 
+import modelos.ClienteAdoptante;
 
 import java.util.*;
-
-/**
- * 
- */
 public class RecordatorioDTO {
 
-    /**
-     * Default constructor
-     */
-    public RecordatorioDTO() {
+    private ClienteAdoptante destinatario;
+    private String mensaje;
+    private Date fecha;
+    public RecordatorioDTO(String mensaje, Date fecha, ClienteAdoptante destinatario) {
+        this.mensaje = mensaje;
+        this.fecha = fecha;
+        this.destinatario = destinatario;
     }
-
-    /**
-     * 
-     */
-    public String mensaje;
-
-    /**
-     * 
-     */
-    public Date fecha;
-
-    /**
-     * 
-     */
-    public String destinatario;
-
+    public ClienteAdoptante getDestinatario() {
+        return destinatario;
+    }
+    public String getMensaje() {
+        return mensaje;
+    }
+    public Date getFecha() {
+        return fecha;
+    }
+    public void setDestinatario(ClienteAdoptante destinatario) {
+        this.destinatario = destinatario;
+    }
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
 }
